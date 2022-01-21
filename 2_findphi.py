5da19b4cb9fbb2bb425b3e2d74d358fd6dce9337
MaikeLenz <maikelenz5@gmail.com>
2022-01-21 15:44:59 +0000

    updated exercise 2

--- 2_findphi.py
+++ 2_findphi.py
@@ -11,8 +11,8 @@ L=0.1
 d=0.2
 d_star=d+(n-1)*L
 alpha=0
-tau_21=230*10**(6)
-c=3*10**8
+tau_21=230*10**(-6)
+c=299792458
 V_c=A*d_star
 K = c*sigma_21/V_c
 tau_c=d_star/c*(alpha*L-np.log((r_1*r_2)/2))
@@ -22,7 +22,7 @@ V_g=A*L*n
 #M_2=V_g*N_2 
 
 #want phi(t) (number of photons in cavity)
-def odes(y,t):
+def odes(y,t,K,tau_c,Q_2,tau_21):
     phi=y[0]
     M_2=y[1]
     dphidt= K*M_2*(phi+1)-phi/tau_c
@@ -34,12 +34,16 @@ M_20=0
 phi0=0
 y0=[phi0,M_20]
 
-t=np.linspace(0,100,10000)
-y=odeint(odes,y0,t)
+t=np.linspace(0,0.001,100000)
+y=odeint(odes,y0,t,args=(K,tau_c,Q_2,tau_21))
 
+"""
 phi=y[:,0]
 M_2=y[:,1]
+"""
+phi,M_2=y.T
 
-plt.plot(t,phi)
-plt.plot(t,M_2)
+plt.plot(t,phi,label="phi")
+plt.plot(t,M_2, label="M2")
+plt.legend()
 plt.show()
